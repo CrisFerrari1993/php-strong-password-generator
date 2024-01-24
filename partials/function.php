e050c857bee300415652a00f25f4c7c6bb5b166d
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-24 15:47:45 +0100

    Creazione function.php e spostamento logica su di esso

---
+++ partials/function.php
@@ -0,0 +1,15 @@
+<?php
+function randomPassword($lenght) {
+    if($lenght < 8){
+        return "<span class='d-block text-center' style='color : red'>La password deve contenere almeno 8 caratteri</span>";
+    }
+    $alphabetCaracters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*<>?/.-_';
+    $newPsw = array();
+    $alphaLength = strlen($alphabetCaracters) - 1;
+    for ($i = 0; $i < $lenght ; $i++) {
+        $n = rand(0, $alphaLength);
+        $newPsw[] = $alphabetCaracters[$n];
+    }
+    return implode($newPsw); 
+}
+?>
